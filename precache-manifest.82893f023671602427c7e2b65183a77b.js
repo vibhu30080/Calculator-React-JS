67d095c718f8519bf04baac8e76d837d684f0967
Khanna <vibkhanna@deloitte.com>
2020-10-12 18:34:05 +0530

    Updates

---
+++ precache-manifest.82893f023671602427c7e2b65183a77b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb5318c48307d4d99af714a9fb24341",
+    "url": "/Calculator-React-JS/index.html"
+  },
+  {
+    "revision": "4910ff54f0241344b816",
+    "url": "/Calculator-React-JS/static/css/main.9cd24ff9.chunk.css"
+  },
+  {
+    "revision": "cbf7108523a79b100a8d",
+    "url": "/Calculator-React-JS/static/js/2.ecca2fe9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-React-JS/static/js/2.ecca2fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4910ff54f0241344b816",
+    "url": "/Calculator-React-JS/static/js/main.42a839e8.chunk.js"
+  },
+  {
+    "revision": "43fc01565b54afc64d3e",
+    "url": "/Calculator-React-JS/static/js/runtime-main.5ae8130f.js"
+  }
+]);
